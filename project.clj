16d1b69372802b847eacfe7f35d4267e967cc980
Nikita Prokopov <prokopov@gmail.com>
2016-08-31 02:52:57 +0700

    added :url to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject clojure-future-spec "1.9.0-alpha11"
   :description "Backport of clojure.spec for Clojure 1.8"
+  :url "https://github.com/tonsky/clojure-future-spec"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
